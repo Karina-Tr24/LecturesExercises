397f09dc95434ba81d0645a1f495480bb2137d9e
B149311-2019 <s1988593@ed.ac.uk>
2019-09-28 17:44:34 +0100

    sorts people by birthday month in files and writes name and birthday

--- Lecture04/commands.sh
+++ Lecture04/commands.sh
@@ -1,19 +1,16 @@
+rm -f *.details
 count=0
 IFS=$'\t'
 while read name email city birthday_day birthday_month birthday_year country;
 do
-	if test -z $birthday_month
+	if test -z $birthday_month || test $birthday_month = "birthday_month"
 	then 
-	echo -e "X\tNO data"
-	else 
-	if test $birthday_month = "birthday_month"
-	then 
-	echo -e "X\tHeader line"
+	echo -e "There is no file"	
 	else
 	count=$((count+1)); 
-	echo -e "$count \t $birthday_month"; 
-fi # $name
-fi # header
-done<example_people_data.tsv
+	echo -e  "$count\t$name\t$birthday_day\t$birthday_month\t$birthday_year" >> $birthday_month.details;
+	 
+fi #names and header
+done<example_people_data.tsv | head -10
 
 
