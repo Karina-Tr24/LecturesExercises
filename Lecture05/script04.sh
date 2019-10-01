ca029783233670f4906b675ce0c624c1a95fccce
B149311-2019 <s1988593@ed.ac.uk>
2019-10-01 11:46:54 +0100

    list HSP lecture05 just like the one before but this put into a file

--- Lecture05/script04.sh
+++ Lecture05/script04.sh
@@ -1,9 +1,13 @@
-
+rm -f *.details
 IFS=$'\t'
 count=0 
 while read querryac subjectac identity alignmentlength mismatches gapopens q_start q_end s_start s_end evalue bitscore
  do 	
+ if !(test -z $subjectac)
+ then
  count=$((count +1))
- echo -e "$count\t$subjectac"
-done < blastoutput2.out
+ echo 	-e "$count\t$subjectac" >> list.details
+fi #for get rid of blanck spaces
+done < blastoutput2.out | tail -20
+
 
