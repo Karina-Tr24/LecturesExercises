619dfa3c5c79cc2a2449aa6b0a59bd0141754da5
Karina-Tr24 <email_not_provided>
2019-10-29 12:55:21 +0000

    lecture13

---
+++ Lecture13/kmers.py
@@ -0,0 +1,8 @@
+
+#!/usr/bin/python3
+import os, sys, subprocess
+
+def kmers(seq,ksize=2,minkfreq=3):
+	if ksize>len(seq):
+	return "Sorry your kmer length is longer that actual dna sequence"
+	if 
