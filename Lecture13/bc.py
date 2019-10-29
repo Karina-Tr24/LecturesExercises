833d3eb90aa2aa423a4dc5c6df5e43dd8ebce65b
Karina-Tr24 <email_not_provided>
2019-10-29 12:09:16 +0000

    base counter

---
+++ Lecture13/bc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import os, sys, subprocess
+
+def undet(seq):
+	length=len(seq)
+	bp=seq.count('t') + seq.count('a') + seq.count('c') + seq.count('g')
+	per=(bp/length)*100
+	if per>40:
+		TF='True'
+	else:
+		TF='False'
+	return TF
+
+assert undet("atcggtcsatcscscsca") == 'True'
+assert undet("aaatggcagctacxxxxxggggaaattcgcgcgcgc") == 'True'
+assert undet("acatctactagggxxxxxxxxddddeeeee") == 'False'
