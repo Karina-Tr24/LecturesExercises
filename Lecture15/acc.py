01c13ed921499eb3b05bd06d34e1a2018875c801
Karina-Tr24 <email_not_provided>
2019-11-05 11:20:08 +0000

    looks for d or e in acc number

--- Lecture15/acc.py
+++ Lecture15/acc.py
@@ -6,3 +6,8 @@ accnum=['xkn59438', 'yhdck2', 'eihd39d9', 'chdsye847', 'hedle3455', 'xjhd53e', '
 for num in accnum:
   if re.search(r'5',num):
     print('Found a 5 in acc number: ' + num)
+
+print('\n')    
+for num in accnum:
+  if re.search(r'[d|e]',num):
+    print('Found d or e in acc number: ' + num)
