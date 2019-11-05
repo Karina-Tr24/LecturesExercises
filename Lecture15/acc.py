1c6f6433e5254820bd1a9ee3f34393c4527a586d
Karina-Tr24 <email_not_provided>
2019-11-05 11:16:27 +0000

    looks for 5 in acc number

---
+++ Lecture15/acc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import os, sys, subprocess, shutil, re
+
+accnum=['xkn59438', 'yhdck2', 'eihd39d9', 'chdsye847', 'hedle3455', 'xjhd53e', '45da', 'de37dp']
+
+for num in accnum:
+  if re.search(r'5',num):
+    print('Found a 5 in acc number: ' + num)
