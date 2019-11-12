7c7394953b4c69bcb49305dd4703bf95112e6918
Karina-Tr24 <email_not_provided>
2019-11-12 11:48:34 +0000

    first question done

--- Lecture17/exercise1.py
+++ Lecture17/exercise1.py
@@ -7,5 +7,7 @@ import pandas as pd
 df = pd.read_csv('/localdisk/data/BPSM/Lecture17_AI/eukaryotes.txt', sep="\t")
 
 #how many fungal species have genomes bigger than 100Mb
-fungi=df[df.apply( lambda x : x['Size (Mb)'] > 100 and x['Group'] in ['Fungi'], axis=1 )].shape[0]
-print(fungi) 
+fungi=df[df.apply( lambda x : x['Size (Mb)'] > 100 and x['Group'] in ['Fungi'], axis=1 )]
+print(fungi.shape[0]) 
+#Set of names of fungi sequences
+print(set(df[df.apply( lambda x : x['Size (Mb)'] > 100 and x['Group'] in ['Fungi'], axis=1 )]['#Organism/Name']))
